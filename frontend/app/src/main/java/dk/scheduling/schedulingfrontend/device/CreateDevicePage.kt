b4df500539cadf60b68169b54cc1a0c1f3d9d24a
Puvikaran Santhirasegaram <74664443+Puvikaran2001@users.noreply.github.com>
2024-04-05 13:02:29 +0200

    Done

--- frontend/app/src/main/java/dk/scheduling/schedulingfrontend/device/CreateDevicePage.kt
+++ frontend/app/src/main/java/dk/scheduling/schedulingfrontend/device/CreateDevicePage.kt
@@ -1,21 +1,14 @@
 package dk.scheduling.schedulingfrontend.device
 
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.border
-import androidx.compose.foundation.interaction.MutableInteractionSource
-import androidx.compose.foundation.interaction.collectIsFocusedAsState
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material3.ButtonDefaults.buttonColors
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -23,15 +16,15 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import dk.scheduling.schedulingfrontend.sharedcomponents.StandardTextField
+import androidx.compose.ui.unit.em
 import dk.scheduling.schedulingfrontend.sharedcomponents.FilledButton
 import dk.scheduling.schedulingfrontend.sharedcomponents.OutlinedButton
+import dk.scheduling.schedulingfrontend.sharedcomponents.StandardTextField
 
 @Composable
 fun CreateDevicePage(
@@ -46,6 +39,23 @@ fun CreateDevicePage(
     var isEffectSet by remember {
         mutableStateOf(true)
     }
+    Column(
+        modifier =
+            modifier
+                .fillMaxSize()
+                .padding(all = 50.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Top,
+    ) {
+        Spacer(modifier = Modifier.height(90.dp))
+        Text(
+            text = "Create a Device",
+            fontSize = 7.em,
+            textAlign = TextAlign.Center,
+            color = MaterialTheme.colorScheme.primary,
+            fontWeight = FontWeight(700),
+        )
+    }
 
     Column(
         modifier =
@@ -59,9 +69,7 @@ fun CreateDevicePage(
             modifier = modifier,
             "Device Name",
             device.name,
-            onValueChange = {
-                device = Device(-1, it, device.effect)
-            },
+            onValueChange = { device = Device(-1, it, device.effect) },
         )
 
         Spacer(modifier = Modifier.height(20.dp))
@@ -111,10 +119,9 @@ fun CreateDevicePage(
 }
 
 fun createDevice(device: Device): Boolean {
+    // Send info to the server and if all goes well return true otherwise false
     return false
 }
-
-
 
 @Preview(showBackground = true, device = "spec:id=reference_phone,shape=Normal,width=411,height=891,unit=dp,dpi=420")
 @Composable
--- frontend/app/src/main/java/dk/scheduling/schedulingfrontend/sharedcomponents/TextFields.kt
+++ frontend/app/src/main/java/dk/scheduling/schedulingfrontend/sharedcomponents/TextFields.kt
@@ -9,18 +9,7 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-
-@Composable
-fun TextLabel(label: String) {
-    Text(
-        label,
-        style = TextStyle(fontSize = 15.sp),
-    )
-}
 
 @Composable
 fun StandardTextField(
@@ -32,17 +21,17 @@ fun StandardTextField(
     isError: Boolean = false,
 ) {
     Column {
-        TextLabel(label)
+        Text(label)
         TextField(
             value = value,
             onValueChange = onValueChange,
             modifier =
-            modifier
-                .fillMaxWidth()
-                .border(
-                    color = if (isError) MaterialTheme.colorScheme.error else Color.Black,
-                    width = 1.dp,
-                ),
+                modifier
+                    .fillMaxWidth()
+                    .border(
+                        color = if (isError) MaterialTheme.colorScheme.error else MaterialTheme.colorScheme.primary,
+                        width = 1.dp,
+                    ),
             singleLine = true,
             keyboardOptions = keyboardOptions,
         )
