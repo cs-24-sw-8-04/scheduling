e0cdce71a991f1bce3f67597efa7a3b84932df6c
S3JER <72788875+S3JER@users.noreply.github.com>
2024-04-05 13:23:13 +0200

    linter

--- frontend/app/src/main/java/dk/scheduling/schedulingfrontend/api/protocol/Events.kt
+++ frontend/app/src/main/java/dk/scheduling/schedulingfrontend/api/protocol/Events.kt
@@ -3,7 +3,8 @@ package dk.scheduling.schedulingfrontend.api.protocol
 data class Event(
     val id: Long,
     val task_id: Long,
-    val start_time: String, // TODO: Change to Date
+    // TODO: Change to Date
+    val start_time: String,
 )
 
 data class GetEventsResponse(
