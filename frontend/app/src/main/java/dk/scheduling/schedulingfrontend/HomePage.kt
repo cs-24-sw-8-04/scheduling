d0ee0e6a70dbbcf0b8b82ca56b3ba71a451fc999
Mads Mogensen <mail@madsmogensen.dk>
2024-04-12 09:09:39 +0200

    Fix small issues
    Use ElevatedCard for the device cards.
    Fix issue where left and right parts of the card's shadow is cut off.
    Clean up the DeviceCard to not use spacers.
    Move the delete button into the expanded area in the lower right corner.
    Animate the expansion and contraction of the card.
    
    Use a scaffold as the root node of the app.
    This provides the bottom bar, background color and other niceties.
    
    Change pagesInfo list to a map.

--- frontend/app/src/main/java/dk/scheduling/schedulingfrontend/HomePage.kt
+++ frontend/app/src/main/java/dk/scheduling/schedulingfrontend/HomePage.kt
@@ -1,30 +1,25 @@
 package dk.scheduling.schedulingfrontend
 
+import androidx.compose.animation.animateContentSize
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material3.AlertDialog
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
@@ -64,7 +59,7 @@ fun HomePagePreviewLightMode() {
     }
 }
 
-@Preview(showBackground = true, device = "spec:id=reference_phone,shape=Normal,width=411,height=891,unit=dp,dpi=420")
+@Preview(showBackground = false, device = "spec:id=reference_phone,shape=Normal,width=411,height=891,unit=dp,dpi=420")
 @Composable
 fun HomePagePreviewDarkMode() {
     SchedulingFrontendTheme(darkTheme = true, dynamicColor = false) {
@@ -82,8 +77,7 @@ fun HomePage(
     LazyColumn(
         modifier =
             modifier
-                .fillMaxSize()
-                .padding(horizontal = 20.dp),
+                .fillMaxSize(),
     ) {
         items(devices) { deviceOverview ->
             DeviceCard(
@@ -110,75 +104,60 @@ fun DeviceCard(
 
     val deviceState by remember { mutableStateOf(getDeviceState(deviceOverview)) }
 
-    Card(
-        shape = RoundedCornerShape(5.dp),
-        elevation = CardDefaults.cardElevation(defaultElevation = 8.dp),
+    ElevatedCard(
         modifier =
             Modifier
-                .padding(vertical = 8.dp)
-                .fillMaxWidth(),
+                .animateContentSize()
+                .fillMaxWidth()
+                .padding(8.dp),
     ) {
-        val modifier = Modifier.padding(horizontal = 10.dp)
-
-        Row(
+        Column(
             modifier =
-                modifier
+                Modifier
                     .fillMaxSize()
-                    .padding(vertical = 8.dp)
-                    .clickable(
-                        onClick = { expandedCard = !expandedCard },
-                    ),
-            verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.SpaceBetween,
+                    .padding(12.dp),
         ) {
             Row(
                 modifier =
-                    Modifier.fillMaxHeight(),
+                    Modifier
+                        .fillMaxSize()
+                        .clickable(
+                            onClick = { expandedCard = !expandedCard },
+                        ),
                 verticalAlignment = Alignment.CenterVertically,
             ) {
-                DeviceStateIcon(deviceState)
+                Column {
+                    DeviceStateIcon(deviceState)
+                }
 
-                Text(
-                    text = deviceOverview.device.name,
-                    modifier = Modifier.padding(8.dp),
-                )
-            }
-
-            Icon(
-                modifier = Modifier.rotate(rotationState),
-                imageVector = Icons.Default.ArrowDropDown,
-                contentDescription = "Drop-Down Arrow",
-            )
-        }
-
-        if (expandedCard) {
-            HorizontalDivider(
-                thickness = 2.dp,
-                color = MaterialTheme.colorScheme.onPrimaryContainer,
-            )
-            Spacer(modifier = Modifier.height(7.dp))
-
-            Row(
-                modifier = modifier.fillMaxWidth(),
-                horizontalArrangement = Arrangement.SpaceBetween,
-            ) {
-                Column(
-                    modifier = Modifier.fillMaxWidth(0.8f),
-                ) {
-                    DeviceInfo(device = deviceOverview.device)
-
-                    Spacer(modifier = Modifier.height(7.dp))
-
-                    DeviceStatus(
-                        deviceOverview = deviceOverview,
-                        state = deviceState,
+                Column {
+                    Text(
+                        text = deviceOverview.device.name,
+                        modifier =
+                            Modifier.padding(8.dp),
                     )
                 }
 
-                Column(
-                    modifier = Modifier.fillMaxWidth(0.5f),
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                ) {
+                Spacer(Modifier.weight(1f))
+
+                Column {
+                    Icon(
+                        modifier = Modifier.rotate(rotationState),
+                        imageVector = Icons.Default.ArrowDropDown,
+                        contentDescription = "Drop-Down Arrow",
+                    )
+                }
+            }
+            if (expandedCard) {
+                DeviceInfo(modifier = Modifier, deviceOverview.device)
+
+                DeviceStatus(
+                    modifier = Modifier,
+                    deviceOverview = deviceOverview,
+                    state = deviceState,
+                )
+
+                Box(modifier = Modifier.fillMaxWidth().padding(0.dp), contentAlignment = Alignment.BottomEnd) {
                     DeleteDeviceIconButton(
                         device = deviceOverview.device,
                         onRemove = {
@@ -188,8 +167,6 @@ fun DeviceCard(
                     )
                 }
             }
-
-            Spacer(modifier = Modifier.height(7.dp))
         }
     }
 }
@@ -269,6 +246,7 @@ fun DeviceStatus(
 fun DeleteDeviceIconButton(
     device: Device,
     onRemove: () -> Unit,
+    modifier: Modifier = Modifier,
 ) {
     var openConfirmDialog by remember { mutableStateOf(false) }
 
