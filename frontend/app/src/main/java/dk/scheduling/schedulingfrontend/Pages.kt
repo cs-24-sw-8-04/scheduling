6c4448b6f687a4cfb6d56a03f72c2b6ac195c1c6
Mads Mogensen <mail@madsmogensen.dk>
2024-04-12 09:09:39 +0200

    Fix small issues
    Use ElevatedCard for the device cards.
    Fix issue where left and right parts of the card's shadow is cut off.
    Clean up the DeviceCard to not use spacers.
    Move the delete button into the expanded area in the lower right corner.
    Animate the expansion and contraction of the card.
    
    Use a scaffold as the root node of the app.
    This provides the bottom bar, background color and other niceties.
    
    Change pagesInfo list to a map.

--- frontend/app/src/main/java/dk/scheduling/schedulingfrontend/Pages.kt
+++ frontend/app/src/main/java/dk/scheduling/schedulingfrontend/Pages.kt
@@ -5,7 +5,9 @@ import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Settings
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
+import testdata.testDeviceOverview
 
 enum class PageNumber {
     HomePage,
@@ -14,15 +16,18 @@ enum class PageNumber {
 }
 
 data class PageInfo(
-    val pageNumber: PageNumber,
     val icon: ImageVector,
     val description: String,
     val composable: @Composable () -> Unit,
 )
 
 val pagesInfo =
-    listOf(
-        PageInfo(PageNumber.HomePage, Icons.Default.Home, "Home") { HomePage() },
-        PageInfo(PageNumber.ApiButtonPage, Icons.Default.Favorite, "Favorite") { ApiButton() },
-        PageInfo(PageNumber.PAGE_3, Icons.Default.Settings, "Settings") { Page3() },
+    mapOf(
+        PageNumber.HomePage to
+            PageInfo(
+                Icons.Default.Home,
+                "Home",
+            ) { HomePage(modifier = Modifier, getDevices = { testDeviceOverview() }) },
+        PageNumber.ApiButtonPage to PageInfo(Icons.Default.Favorite, "Favorite") { ApiButton() },
+        PageNumber.PAGE_3 to PageInfo(Icons.Default.Settings, "Settings") { Page3() },
     )
