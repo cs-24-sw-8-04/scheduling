c8f34565e44aeea30065e3dc9f20f7e8605708f6
henneboy <72788863+henneboy@users.noreply.github.com>
2024-04-16 14:31:07 +0200

    Line was to long, which apparently is completely unacceptable....

--- frontend/app/src/main/java/dk/scheduling/schedulingfrontend/pages/CreateTaskPage.kt
+++ frontend/app/src/main/java/dk/scheduling/schedulingfrontend/pages/CreateTaskPage.kt
@@ -197,7 +197,8 @@ fun isValidInput(
     endTime: TimePickerState,
 ): Boolean {
     // Max line length is violated if not split into two expressions.
-    val startIsBeforeEnd = dateRange.getStartDate() != dateRange.getEndDate() || startTime.hour <= endTime.hour && startTime.minute < endTime.minute
+    val startTimeIsBeforeEndTime = startTime.hour <= endTime.hour && startTime.minute < endTime.minute
+    val startIsBeforeEnd = dateRange.getStartDate() != dateRange.getEndDate() || startTimeIsBeforeEndTime
     return isValidDuration(
         duration,
     ) && dateRange.isValidRange() && startIsBeforeEnd
