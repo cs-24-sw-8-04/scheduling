2d70a8dc615a370b4fc58c4034ed92ca1024b7ef
Puvikaran Santhirasegaram <74664443+Puvikaran2001@users.noreply.github.com>
2024-04-18 12:08:27 +0200

    renaming

--- frontend/app/src/main/java/dk/scheduling/schedulingfrontend/exceptions/UserNotLoggedInException.kt
+++ frontend/app/src/main/java/dk/scheduling/schedulingfrontend/exceptions/UserNotLoggedInException.kt
@@ -1,3 +1,3 @@
 package dk.scheduling.schedulingfrontend.exceptions
 
-class UserNotLoggedInException(message: String = "User is not logged in", val exception: Throwable? = null) : Exception(message)
+class UserNotLoggedInException(message: String = "User is not logged in", val other_exception: Throwable? = null) : Exception(message)
