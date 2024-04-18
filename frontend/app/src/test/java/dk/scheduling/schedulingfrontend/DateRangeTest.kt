a030add59deb09f9ac29dc409e92dc42a6808e03
henneboy <72788863+henneboy@users.noreply.github.com>
2024-04-18 12:45:07 +0200

    Using built-in data classes

--- frontend/app/src/test/java/dk/scheduling/schedulingfrontend/DateRangeTest.kt
+++ frontend/app/src/test/java/dk/scheduling/schedulingfrontend/DateRangeTest.kt
@@ -9,8 +9,8 @@ class DateRangeTest {
         val date1 = DateRange(0, Long.MAX_VALUE)
         assert(date1.isValidRange()) { "Valid range" }
 
-        assert(date1.getStartDate() == "01/01/1970") { "Should equal epoch date, not ${date1.getStartDate()}" }
-        assert(date1.getEndDate() == "17/08/292278994") { "Should equal 17/08/292278994, not ${date1.getEndDate()}" }
+        assert(date1.getStartDate() == "1970-01-01") { "Should equal epoch date, not ${date1.getStartDate()}" }
+        assert(date1.getEndDate() == "+292278994-08-17") { "Should equal 17-08-292278994, not ${date1.getEndDate()}" }
 
         val date2 = DateRange(Long.MAX_VALUE, Long.MIN_VALUE)
         assert(!date2.isValidRange()) { "Invalid range, startTime > endTime" }
