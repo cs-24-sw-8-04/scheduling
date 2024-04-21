68903f3476df1961b172a4576d1fda075bee123d
Puvikaran Santhirasegaram <74664443+Puvikaran2001@users.noreply.github.com>
2024-04-21 22:24:08 +0200

    Renaming EventTask to TaskEvent

--- frontend/app/src/main/java/testdata/DeviceTaskTestData.kt
+++ frontend/app/src/main/java/testdata/DeviceTaskTestData.kt
@@ -1,19 +1,19 @@
 package testdata
 
 import dk.scheduling.schedulingfrontend.model.DeviceTask
-import dk.scheduling.schedulingfrontend.model.EventTask
+import dk.scheduling.schedulingfrontend.model.TaskEvent
 import java.time.LocalDateTime
 
 fun deviceTaskTestData(dateTime: LocalDateTime = LocalDateTime.now()): List<DeviceTask> {
     val dictionaryEvents = eventsTestData(dateTime).associateBy { it.task_id }
 
-    val eventTasks: MutableList<EventTask> = mutableListOf()
-    tasksTestData(dateTime).mapTo(eventTasks) {
+    val taskEvents: MutableList<TaskEvent> = mutableListOf()
+    tasksTestData(dateTime).mapTo(taskEvents) {
         val event = dictionaryEvents[it.id]
-        EventTask(it, event)
+        TaskEvent(it, event)
     }
 
-    val groupTaskEventsToDevice = eventTasks.groupBy { it.task.device_id }
+    val groupTaskEventsToDevice = taskEvents.groupBy { it.task.device_id }
 
     val deviceTasks: MutableList<DeviceTask> = mutableListOf()
 
