e453044fbc162e0d3e5900eccfee63595fff2911
Puvikaran Santhirasegaram <74664443+Puvikaran2001@users.noreply.github.com>
2024-04-21 23:11:23 +0200

    Fix klint and Refactor

--- frontend/app/src/main/java/dk/scheduling/schedulingfrontend/model/DeviceOverview.kt
+++ frontend/app/src/main/java/dk/scheduling/schedulingfrontend/model/DeviceOverview.kt
@@ -2,7 +2,6 @@ package dk.scheduling.schedulingfrontend.model
 
 import dk.scheduling.schedulingfrontend.api.protocol.Device
 
-
 data class DeviceOverview(
     var device: Device,
     var taskEvent: TaskEvent?,
--- frontend/app/src/main/java/testdata/DeviceOverviewTestData.kt
+++ frontend/app/src/main/java/testdata/DeviceOverviewTestData.kt
@@ -8,12 +8,7 @@ fun testDeviceOverview(dateTime: LocalDateTime = LocalDateTime.now()): List<Devi
 
     val dest: MutableList<DeviceOverview> = mutableListOf()
     deviceTasks.mapTo(dest) {
-        val taskEvent = if (it.tasks.isNotEmpty()) {
-            if (it.tasks[0].event != null) {
-                it.tasks[0]
-            }
-            else null
-        } else null
+        val taskEvent = it.tasks.firstOrNull()?.takeIf { taskEvent -> taskEvent.event != null }
         DeviceOverview(it.device, taskEvent)
     }
 
