a48cfcdb32442eba57162e90beaf2c84005525c5
Puvikaran Santhirasegaram <74664443+Puvikaran2001@users.noreply.github.com>
2024-04-26 09:13:13 +0200

    Fix Ktlint

--- frontend/app/src/main/java/dk/scheduling/schedulingfrontend/components/Loading.kt
+++ frontend/app/src/main/java/dk/scheduling/schedulingfrontend/components/Loading.kt
@@ -3,7 +3,6 @@ package dk.scheduling.schedulingfrontend.components
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
 import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -16,10 +15,8 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import dk.scheduling.schedulingfrontend.pages.LoginPage
 import dk.scheduling.schedulingfrontend.ui.theme.SchedulingFrontendTheme
 import kotlinx.coroutines.delay
-import testdata.DummyAccountRepository
 
 @Composable
 fun Loading(
@@ -73,4 +70,3 @@ fun LoadingPreviewLightMode() {
         }
     }
 }
-
