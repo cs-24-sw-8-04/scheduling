31f489cb25dbeb6efdcf8e58879df98220488991
henneboy <72788863+henneboy@users.noreply.github.com>
2024-04-26 13:04:56 +0200

    Same sleep duration for all methods

--- frontend/app/src/main/java/testdata/DummyDeviceRepository.kt
+++ frontend/app/src/main/java/testdata/DummyDeviceRepository.kt
@@ -3,6 +3,7 @@ package testdata
 import dk.scheduling.schedulingfrontend.api.protocol.Device
 import dk.scheduling.schedulingfrontend.repositories.device.IDeviceRepository
 
+@Suppress("BlockingMethodInNonBlockingContext")
 class DummyDeviceRepository : IDeviceRepository {
     private val devices: MutableList<Device> =
         mutableListOf(
@@ -12,8 +13,10 @@ class DummyDeviceRepository : IDeviceRepository {
         )
 
     private var nextDeviceId: Long = 4
+    private val sleepDuration: Long = 2000
 
     override suspend fun getAllDevices(): List<Device> {
+        Thread.sleep(sleepDuration)
         return devices.toList()
     }
 
@@ -21,10 +24,12 @@ class DummyDeviceRepository : IDeviceRepository {
         name: String,
         effect: Double,
     ) {
+        Thread.sleep(sleepDuration)
         devices.add(Device(nextDeviceId++, name, effect))
     }
 
     override suspend fun deleteDevice(deviceId: Long) {
+        Thread.sleep(sleepDuration)
         devices.removeAll { device: Device -> device.id == deviceId }
     }
 }
