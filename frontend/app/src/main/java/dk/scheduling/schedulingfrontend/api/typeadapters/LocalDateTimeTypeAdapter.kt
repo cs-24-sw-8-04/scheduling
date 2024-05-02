68794dfbd8e947cf602843bd958619fe2b9fdb31
Puvikaran Santhirasegaram <74664443+Puvikaran2001@users.noreply.github.com>
2024-05-02 14:57:46 +0200

    Fix serialize localdatetime

--- frontend/app/src/main/java/dk/scheduling/schedulingfrontend/api/typeadapters/LocalDateTimeTypeAdapter.kt
+++ frontend/app/src/main/java/dk/scheduling/schedulingfrontend/api/typeadapters/LocalDateTimeTypeAdapter.kt
@@ -1,5 +1,6 @@
 package dk.scheduling.schedulingfrontend.api.typeadapters
 
+import android.util.Log
 import com.google.gson.JsonDeserializationContext
 import com.google.gson.JsonDeserializer
 import com.google.gson.JsonElement
@@ -12,6 +13,7 @@ import java.time.Instant
 import java.time.LocalDateTime
 import java.time.ZoneId
 import java.time.ZoneOffset
+import java.time.ZonedDateTime
 import java.time.format.DateTimeFormatter
 
 class LocalDateTimeTypeAdapter :
@@ -24,7 +26,19 @@ class LocalDateTimeTypeAdapter :
         typeOfSrc: Type,
         context: JsonSerializationContext,
     ): JsonElement {
-        return JsonPrimitive(datetime.atZone(ZoneOffset.UTC).format(formatter))
+        Log.i("LocalDateTimeTypeAdapter", "serialize - Input datetime: $datetime")
+        val output =
+            ZonedDateTime.ofInstant(
+                datetime.atZone(ZoneId.systemDefault()).toInstant(),
+                ZoneId.of("UTC"),
+            ).toLocalDateTime().atZone(ZoneOffset.UTC).format(formatter)
+
+        Log.i(
+            "LocalDateTimeTypeAdapter",
+            "serialize - Output datetime: $output",
+        )
+
+        return JsonPrimitive(output)
     }
 
     @Throws(JsonParseException::class)
@@ -34,6 +48,8 @@ class LocalDateTimeTypeAdapter :
         context: JsonDeserializationContext,
     ): LocalDateTime {
         val utc = Instant.parse(json.asString)
+        Log.i("LocalDateTimeTypeAdapter", "deserialize - Input datetime: $utc")
+        Log.i("LocalDateTimeTypeAdapter", "deserialize - Output datetime: ${utc.atZone(ZoneId.systemDefault()).toLocalDateTime()}")
         return utc.atZone(ZoneId.systemDefault()).toLocalDateTime()
     }
 }
